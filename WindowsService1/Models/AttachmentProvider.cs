95384968f1720ddf0f193e12b77678519e2c685d
Victor Manuel <vmhl02012003@gmail.com>
2026-02-26 17:03:17 -0600

    Ingresando el proyecto de servicios de windows

---
+++ WindowsService1/Models/AttachmentProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1.Models
+{
+    public class AttachmentProvider
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public long? PaymentRequestID { get; set; }
+        public long? PurchaseOrderID { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedDt { get; set; }
+        public bool? Status { get; set; }
+        public string TypeAttachment { get; set; }
+        public long? RefundOrderID { get; set; }
+        public long? BillingOrderID { get; set; }
+        public string BaseUrl { get; set; }
+        public long? EmployeeID { get; set; }
+    }
+}
